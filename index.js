35038b77c87f348656089854a2a2d419934a7578
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-10-27 00:48:19 +0600

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
+
+app.use(cors())
+
 const port = 5000;
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
